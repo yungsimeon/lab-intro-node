bcc22f0c357f944b7056db59ff1eb4655132351e
Simon Schubert <simonlouis.schubert@gmail.com>
2024-07-08 14:37:05 +0200

    done

--- index.js
+++ index.js
@@ -1,17 +1,50 @@
 class SortedList {
-  constructor() {}
+  constructor() {
+    this.items = [];
+    this.length = 0;
+  }
 
-  add(item) {}
+  add(item) {
+    this.items.push(item);
+    this.items.sort((a, b) => a - b);
+    this.length = this.items.length;
+  }
 
-  get(pos) {}
+  get(pos) {
+    if (pos < 0 || pos >= this.items.length) {
+      throw new Error("OutOfBounds");
+    }
+    return this.items[pos];
+  }
 
-  max() {}
+  max() {
+    if (this.items.length === 0) {
+      throw new Error("EmptySortedList");
+    }
+    return Math.max(...this.items);
+  }
 
-  min() {}
+  min() {
+    if (this.items.length === 0) {
+      throw Error("EmptySortedList");
+    }
+    return Math.min(...this.items);
+  }
 
-  sum() {}
+  sum() {
+    if (this.items.length === 0) return 0;
 
-  avg() {}
+    const sum = this.items.reduce((acc, curr) => acc + curr, 0);
+    return sum;
+  }
+
+  avg() {
+    if (this.items.length === 0) {
+      throw Error("EmptySortedList");
+    }
+    const sum = this.items.reduce((acc, curr) => acc + curr, 0);
+    return sum / this.items.length;
+  }
 }
 
 module.exports = SortedList;
